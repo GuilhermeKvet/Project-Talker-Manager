fcdf62cbe51f97b9f83294f92a4798841737d12c
Guilherme Vinicius Kvet da Rocha <guilhermevkvet22@gmail.com>
2022-08-26 19:45:31 -0300

    Realizando requisito 4

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,23 +1,18 @@
 const express = require('express');
+const crypto = require('crypto');
+const validateLogin = require('../middleware/validateLogin');
 
 const router = express.Router();
 
-const getToken = () => {
-  let token = '';
-  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let index = 0; index < 16; index += 1) {
-    token += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
-  }
-  return token;
-};
+const getToken = () => crypto.randomBytes(8).toString('hex');
 
-router.post('/', (req, res) => {
+router.post('/', validateLogin, (req, res) => {
   const { email, password } = req.body;
   if (email && password) {
     const randomToken = getToken();
     return res.status(200).json({ token: randomToken });
   }
-  return res.status(404).json({ message: 'Insira os valores de email e senha' });
+  return res.status(400).json({ message: 'Insira os valores de email e senha' });
 });
 
 module.exports = router;
